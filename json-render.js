e90307c3a10b7e5aa0c2fe8d52f46b0787383704
jtholm2 <jtholmes@DESKTOP-0ECUR6T.localdomain>
2020-10-14 17:31:36 -0700

    Practiced querying an API and displaying JSON data in a browser utilizing ExpressJS

---
+++ json-render.js
@@ -0,0 +1,18 @@
+const { response } = require('express');
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.get('/isslocation', (req, res) => {
+    fetch('http://api.open-notify.org/iss-now.json')
+        .then(res => res.text())
+        .then(text => res.send(text));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
