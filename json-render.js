83b1db2677b8a9344689af78764e126da6882a91
jtholm2 <jtholmes@DESKTOP-0ECUR6T.localdomain>
2020-10-21 16:38:46 -0700

    Added functionality when users browse to /isslocation. It will retrieve current geo-data and display it utilizing the leaflet library.

--- json-render.js
+++ json-render.js
@@ -1,16 +1,16 @@
+const { request } = require('express');
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
+var path = require('path');
 const port = 3000;
-
 app.use(express.json());
 
 
 app.get('/isslocation', (req, res) => {
-    fetch('http://api.open-notify.org/iss-now.json')
-        .then(res => res.text())
-        .then(text => res.send(text));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
