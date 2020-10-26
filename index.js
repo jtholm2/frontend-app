9c386cc457758f44843796d7fa1456a2235e3b64
jtholm2 <jtholmes@DESKTOP-0ECUR6T.localdomain>
2020-10-26 13:24:19 -0700

    Added generic addresses for future use by other individuals.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const { request } = require('express');
 const express = require('express');
-const fetch = require('node-fetch');
 const app = express();
 var path = require('path');
 const port = 3000;
 app.use(express.json());
 
 
-app.get('/isslocation', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
--- store-fetch.js
+++
@@ -1,1 +0,0 @@
-const { Connection, Request } = require("tedious");
