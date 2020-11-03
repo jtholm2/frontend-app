278a1f163c337d704f8c5ba3e6ba82e886b03c09
jtholm2 <jtholmes@DESKTOP-0ECUR6T.localdomain>
2020-11-03 15:08:22 -0800

    Added functionality to serve a Blob file via the backend application. Look to storage.html for updates.

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/azurestorage', (req, res) => {
+    res.sendFile(path.join(__dirname + '/storage.html'));
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   });
