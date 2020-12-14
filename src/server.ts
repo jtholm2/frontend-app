cebf4e05a19f4a2256a71cd5b948c3680496c1d4
JT Holmes <jtholmes@microsoft.com>
2020-12-14 22:26:11 +0000

    Merged PR 5: Created the appropriate files needed to properly build this application.
    Created the appropriate files needed to properly build this application.

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-const { request } = require('express');
-const express = require('express');
+import express from 'express';
 const app = express();
-var path = require('path');
+import path from 'path';
 const port = 3000;
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.get('/azurestorage', (req, res) => {
-    res.sendFile(path.join(__dirname + '/storage.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'storage.html'));
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   });
+
