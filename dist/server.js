64017e0dab54f06dba6655bb2a1f125f6b7c5c41
JT Holmes <jtholmes@microsoft.com>
2020-12-23 02:21:33 +0000

    Merged PR 17: Ran build and added files to repo
    Ran build and added files to repo

---
+++ dist/server.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const path_1 = __importDefault(require("path"));
+const port = process.env.PORT;
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '..', 'public', 'index.html'));
+});
+app.get('/azurestorage', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '..', 'public', 'storage.html'));
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+//# sourceMappingURL=server.js.map
